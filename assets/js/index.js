890c8234f3a9ac313598c3b6ca879e3555c94b9a
Dev Avology <devavology12@gmail.com>
2026-06-08 18:50:00 +0530

    news page added

--- assets/js/index.js
+++ assets/js/index.js
@@ -129,3 +129,20 @@ document.addEventListener("keydown", (e) => {
 });
 
 // Home Videos JS  End
+
+// News Filter JS Start
+function filterCards(category, btn) {
+  document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
+  btn.classList.add('active');
+
+
+  document.querySelectorAll('.news_card').forEach(card => {
+      if (category === 'all' || card.classList.contains(category)) {
+          card.style.display = 'block';
+      } else {
+          card.style.display = 'none';
+      }
+  });
+}
+
+// News Filter JS End
